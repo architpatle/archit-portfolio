71d696aaf1763a05148e5c0832fcbed16ae96478
Archit Patle <architpatle06@gmail.com>
2026-05-16 15:06:43 +0530

    Updated footer by adding contact no. and rearranging the options

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,79 +4,111 @@ import {
   FiGithub,
   FiLinkedin,
   FiMail,
+  FiPhone,
 } from "react-icons/fi";
 
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function Footer() {
-
   return (
     <footer className={styles.footer}>
 
       <div className={styles.wrapper}>
 
-        {/* LEFT */}
-        <div className={styles.left}>
+        {/* TOP */}
+        <div className={styles.top}>
 
-          <h3 className={styles.brand}>
-            Archit Patle
-          </h3>
+          {/* COLUMN 1 */}
+          <div className={styles.column}>
 
-          <p className={styles.text}>
-            Frontend developer crafting modern,
-            scalable and immersive web experiences.
-          </p>
+            <h3 className={styles.brand}>
+              Archit Patle
+            </h3>
 
-          <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=architpatle06@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.email}
-          >
-            <FiMail />
-            <span>architpatle06@gmail.com</span>
-          </a>
+            <p className={styles.text}>
+              Frontend developer crafting modern,
+              scalable and immersive web experiences.
+            </p>
+
+          </div>
+
+          {/* COLUMN 2 */}
+          <div className={styles.column}>
+
+            <h4 className={styles.label}>
+              Contact
+            </h4>
+
+            <a
+              href="mailto:architpatle06@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.contactItem}
+            >
+              <FiMail />
+              <span>architpatle06@gmail.com</span>
+            </a>
+
+            <a
+              href="https://wa.me/918999186683"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.contactItem}
+            >
+              <FiPhone />
+              <span>+91 89991 86683</span>
+            </a>
+
+          </div>
+
+          {/* COLUMN 3 */}
+          <div className={styles.column}>
+
+            <h4 className={styles.label}>
+              Socials
+            </h4>
+
+            <div className={styles.socials}>
+
+              <a
+                href="https://www.linkedin.com/in/archit-patle"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.iconBtn}
+              >
+                <FiLinkedin />
+              </a>
+
+              <a
+                href="https://github.com/architpatle"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.iconBtn}
+              >
+                <FiGithub />
+              </a>
+
+              <a
+                href="https://wa.me/918999186683"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.iconBtn}
+              >
+                <FaWhatsapp />
+              </a>
+
+            </div>
+
+          </div>
 
         </div>
 
-        {/* CENTER */}
-        <div className={styles.center}>
+        {/* BOTTOM */}
+        <div className={styles.bottom}>
 
           <p>
-            © {new Date().getFullYear()} All rights reserved
+            © {new Date().getFullYear()} Archit Patle. All rights reserved.
           </p>
-
-        </div>
-
-        {/* RIGHT */}
-        <div className={styles.right}>
-
-          <a
-            href="https://www.linkedin.com/in/archit-patle"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.iconBtn}
-          >
-            <FiLinkedin />
-          </a>
-
-          <a
-            href="https://github.com/architpatle"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.iconBtn}
-          >
-            <FiGithub />
-          </a>
-
-          <a
-            href="https://wa.me/918999186683"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.topBtn}
-          >
-            <FaWhatsapp />
-          </a>
 
         </div>
 
